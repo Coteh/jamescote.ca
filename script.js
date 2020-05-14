b6ea1800f76eb216c830ad6c76873fff4bd05392
Coteh <3276350+Coteh@users.noreply.github.com>
2020-05-13 20:24:03 -0400

    Add screenshot and README, fix minor visual issues with website

--- script.js
+++ script.js
@@ -20,6 +20,11 @@
     // Highlight the menu link associated with this region by adding the .active CSS class
     $('.main-menu a[href="'+ region +'"]').addClass('active'); 
 
+    // Scroll to top of page
+    setTimeout(function () {
+      window.scrollTo(0, 0);
+    }, 1);
+
     // Alternate method: Use AJAX to load the contents of an external file into a div based on URL fragment
     // This will extract the region name from URL hash, and then load [region].html into the main #content div
     // var region = location.hash.toString() || '#first';
@@ -28,3 +33,29 @@
   });
   
 })(jQuery);
+
+// Function to change background size "cover" to
+// either "100vw auto" (100 percent of viewport
+// width and automatic height)
+// or "auto 100vh" (automatic width and 100
+// percent of viewport height).
+// Adapted from StackOverflow post written by Perttu https://stackoverflow.com/a/48705670
+// Access Date: May 13, 2020
+fixBackgroundSizeCover = function (event) {
+  var bgImageWidth = 5142,
+    bgImageHeight = 3428,
+    bgImageRatio = bgImageWidth / bgImageHeight,
+    windowSizeRatio = window.innerWidth / window.innerHeight;
+
+  if (bgImageRatio > windowSizeRatio) {
+    document.documentElement.style.backgroundSize = 'auto 100vh';
+  } else {
+    document.documentElement.style.backgroundSize = '100vw auto';
+  }
+};
+
+// Execute the fix function once upon load
+fixBackgroundSizeCover();
+
+// Execute the fix function everytime on window resize
+window.addEventListener('resize', fixBackgroundSizeCover);

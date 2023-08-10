4cac3c468f78895b44432342785d8eafdc29e532
Coteh <3276350+Coteh@users.noreply.github.com>
2023-08-09 22:56:26 -0400

    Fix Projects pane not accessible if JS is disabled
    I did this by making the Projects page part of the index.html (ie. no longer a dynamic pane, since that requires JS to fetch) and then moving the content-region class switching to occur earlier (DOMContentLoaded occurs before load). Both panes are also made visible by default as well so that they will both appear when JS is disabled.

--- script.js
+++ script.js
@@ -82,43 +82,49 @@ showResumePdf = function(clickSource) {
   });
 };
 
+function switchToActivePane() {
+  // First hide all content regions, then show the content-region specified in the URL hash 
+  // (or if no hash URL is found, default to first menu item)
+  $('.content-region').addClass('hide');
+  $('.content-region').removeClass('show');
+  
+  // Remove any active classes on the main-menu
+  $('.main-menu a').removeClass('active');
+  var region = location.hash.toString() || $('.main-menu a:first').attr('href');
+  
+  // Now show the region specified in the URL hash
+  $(region).addClass('show');
+  $(region).removeClass('hide');
+  
+  // Highlight the menu link associated with this region by adding the .active CSS class
+  $('.main-menu a[href="'+ region +'"]').addClass('active'); 
+
+  // Scroll to top of page
+  setTimeout(function () {
+    window.scrollTo(0, 0);
+  }, 1);
+
+  // For sections with "dynamic" class: Use AJAX to load the contents of an external file into the div based on URL fragment
+  // This will extract the region name from URL hash, and then load [region].html into the section div
+  var region = location.hash.toString() || '#first';
+  if ($(region).hasClass('dynamic') && !$(region).hasClass('loaded')) {
+    $(region).load(region.slice(1) + '.html');
+    $(region).addClass('loaded');
+  }
+}
+
 /********************************
   Stuff to run once page loads
 *********************************/
 
 document.addEventListener("DOMContentLoaded", () => {
-  // We use some Javascript and the URL #fragment to hide/show different parts of the page
+  // Display the pane based on current URL #fragment, or the first pane if there's no fragment
+  switchToActivePane();
+
+  // Listen for URL #fragment change to hide/show different parts of the page
   // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#Linking_to_an_element_on_the_same_page
-  $(window).on('load hashchange', function(){
-    
-    // First hide all content regions, then show the content-region specified in the URL hash 
-    // (or if no hash URL is found, default to first menu item)
-    $('.content-region').addClass('hide');
-    $('.content-region').removeClass('show');
-    
-    // Remove any active classes on the main-menu
-    $('.main-menu a').removeClass('active');
-    var region = location.hash.toString() || $('.main-menu a:first').attr('href');
-    
-    // Now show the region specified in the URL hash
-    $(region).addClass('show');
-    $(region).removeClass('hide');
-    
-    // Highlight the menu link associated with this region by adding the .active CSS class
-    $('.main-menu a[href="'+ region +'"]').addClass('active'); 
-
-    // Scroll to top of page
-    setTimeout(function () {
-      window.scrollTo(0, 0);
-    }, 1);
-
-    // For sections with "dynamic" class: Use AJAX to load the contents of an external file into the div based on URL fragment
-    // This will extract the region name from URL hash, and then load [region].html into the section div
-    var region = location.hash.toString() || '#first';
-    if ($(region).hasClass('dynamic') && !$(region).hasClass('loaded')) {
-      $(region).load(region.slice(1) + '.html');
-      $(region).addClass('loaded');
-    }
+  $(window).on('hashchange', function(){
+    switchToActivePane();
   });
 
   // If JS is enabled, then allow for dynamic resume preview without also downloading the resume by preventing link from opening resume pdf
